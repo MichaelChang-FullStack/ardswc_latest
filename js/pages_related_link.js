7fbde699af7bdc17d26fc26a41e642325957e824
LucienLiou <lucien.liou@tarode.com>
2023-07-10 21:34:46 +0800

    Update Related links js

--- js/pages_related_link.js
+++ js/pages_related_link.js
@@ -3,25 +3,27 @@ $(function () {
       try {
         let tableEle = document.getElementById("slider");
         let response = await fetch(
-          "http://localhost:5050/swcb-new/server/related_links.php"
+          "http://localhost:5050/swcb_new/SWCB/server/related_links.php"
         );
         let body = await response.json();
         console.log(body);
   
   
         $.each(body, function (index, item) {
-          console.log(item);
-          console.log(index);
+          /*console.log(item);
+          console.log(index);*/
 
-          if(item.image=='政府網站')
+          if(item.LI_TYPE=='政府網站')
           {
             $("#related_link1").append(
                 '<li><a href="'+item.LI_URL+'">'+item.LI_NAME+'</a></li>');
+                console.log('1');
           }
-          else(item.image=='遊戲')
+          else(item.LI_TYPE=='遊戲')
           {
             $("#related_link2").append(
                 '<li><a href="'+item.LI_URL+'">'+item.LI_NAME+'</a></li>');
+                console.log('2');
           }
 
           
