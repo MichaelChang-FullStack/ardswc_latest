43c6cf0a82d360c7755cec0fed9e2b785c7af783
LucienLiou <lucien.liou@tarode.com>
2023-07-22 23:30:39 +0800

    Add top search

---
+++ js/hot_keyword.js
@@ -0,0 +1,37 @@
+function toTopSearch(data) {
+    const {DM_NAME} = data;
+    return {
+        name: DM_NAME
+    }
+}
+
+async function getTopSearch() {
+    var apiUrl = '/swcb-new/server/hot_keyword.php';
+    try {
+        var response = await fetch(apiUrl)
+        if (!response.ok) {
+            throw new Error('網路請求失敗: ' + response.status);
+        }
+        const data = await response.json();
+        return data.map((d) => toTopSearch(d));
+    } catch (error) {
+        console.error(error)
+    }
+}
+$(document).ready(async function () {
+    const topSearchList = await getTopSearch();
+    topSearchList.forEach(topSearch => {
+        const {name} = topSearch;
+        $('#top_search').append(
+            `
+            <a href='/swcb-new/pages/Search_Result.html?searchText=${name}'>
+                <div class="frequest_search1">
+                    <span>${name}</span>
+                </div>
+            </a>
+                
+            `
+        )
+    });
+    console.log('top', topSearchList)
+})
