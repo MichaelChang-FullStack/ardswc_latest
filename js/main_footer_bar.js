253fb5542f4b98ab9a5f7b4f721653907c340d15
LucienLiou <lucien.liou@tarode.com>
2023-07-23 14:36:59 +0800

    Fix source type link

--- js/main_footer_bar.js
+++ js/main_footer_bar.js
@@ -42,6 +42,11 @@ $(function () {
                     '<li class="lh-lg"><a class="text-200 text-decoration-none" href="/swcb-new/pages/Search_Result.html?sourceType=教案">教案</a></li>'+
                   '</ul>'+
                   '</div>'+
+                  '<div class="footterbar_main_part2">'+
+                    '<ul class="list-unstyled mb-md-4 mb-lg-0">'+
+                      '<li class="lh-lg"><a class="text-200 text-decoration-none" href="/swcb-new/pages/Search_Result.html?sourceType=影片">影片</a></li>'+
+                    '</ul>'+
+                   '</div>'+
                 '</div>'+
                 '</div>'+
                 '<div class="col-6 col-md-4 col-lg-3 col-xxl-2 mb-3">'+
