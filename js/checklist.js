1b51e0e326cd5d12fa565e0d680a3af7c0f55cf0
LucienLiou <lucien.liou@tarode.com>
2023-07-26 11:45:29 +0800

    Fix checklist style a tag to label

--- js/checklist.js
+++ js/checklist.js
@@ -1,5 +1,85 @@
+async function openDefaultFilterList (filterIds) {
+  filterIds.forEach(id => {
+    const checkbox = document.getElementById(`resource${id}`);
+    if(checkbox) {
+      checkbox.checked = true;
+    } 
+  });
+}
+
+function checkDefaultFilterList(searchItemElement, blockId) {
+  var checkboxes = searchItemElement.querySelectorAll('input[type="checkbox"]');
+  checkboxes.forEach(function(checkbox) {
+    if (checkbox.checked) {
+      console.log('Checkbox with ID ' + checkbox.id + ' is checked.');
+      const searchMainMenu = document.querySelector(`.searchitem-${blockId} > .sub-searchmenumain`);
+      searchMainMenu.style.display = 'block';
+      const mainDropdown = document.querySelector(`.dropdown-${blockId}`);
+      mainDropdown.classList.remove('rotatebefore');
+      mainDropdown.classList.add('rotateafter');
+    }
+  });
+}
+
+function checkDefaultSubFilterList(subSearchItemElement, blockId) {
+  var checkboxes = subSearchItemElement.querySelectorAll('input[type="checkbox"]');
+  checkboxes.forEach(function(checkbox) {
+    if (checkbox.checked) {
+      const searchMainMenu = document.querySelector(`.sub-searchitem-${blockId} > .sub-searchmenusub`);
+      searchMainMenu.style.display = 'block';
+      const mainDropdown = document.querySelector(`.sub-dropdown-${blockId}`);
+      mainDropdown.classList.remove('rotatebefore');
+      mainDropdown.classList.add('rotateafter');
+    }
+  })
+}
+
 /*Search Menu Start*/
-$(document).ready(function() {
+$(document).ready(async function() {
+    const queryString = getQueryString();
+    const filterIds = queryString.filterId.split(",");
+    if(filterIds.length > 0) {
+      await openDefaultFilterList(filterIds)
+      console.log(filterIds)
+    }
+      
+    const searchItemElement1 = document.querySelector('.searchitem-1');
+    checkDefaultFilterList(searchItemElement1, 1);
+    //圖書
+    const subSearchItemElement1 = document.querySelector('.sub-searchitem-1');
+    checkDefaultSubFilterList(subSearchItemElement1, 1);
+    //教材
+    const subSearchItemElement2 = document.querySelector('.sub-searchitem-2');
+    checkDefaultSubFilterList(subSearchItemElement2, 2);
+    //教案
+    const subSearchItemElement3 = document.querySelector('.sub-searchitem-3');
+    checkDefaultSubFilterList(subSearchItemElement3, 3);
+    //影片
+    const subSearchItemElement4 = document.querySelector('.sub-searchitem-4');
+    checkDefaultSubFilterList(subSearchItemElement4, 4);
+
+    const searchItemElement2 = document.querySelector('.searchitem-2');
+    checkDefaultFilterList(searchItemElement2, 2);
+    const subSearchItemElement5 = document.querySelector('.sub-searchitem-5');
+    checkDefaultSubFilterList(subSearchItemElement5, 5);
+    const subSearchItemElement6 = document.querySelector('.sub-searchitem-6');
+    checkDefaultSubFilterList(subSearchItemElement6, 6);
+    const subSearchItemElement7 = document.querySelector('.sub-searchitem-7');
+    checkDefaultSubFilterList(subSearchItemElement7, 7);
+
+    const searchItemElement3 = document.querySelector('.searchitem-3');
+    checkDefaultFilterList(searchItemElement3, 3);
+
+    const searchItemElement4 = document.querySelector('.searchitem-4');
+    checkDefaultFilterList(searchItemElement4, 4);
+
+    const searchItemElement5 = document.querySelector('.searchitem-5');
+    checkDefaultFilterList(searchItemElement5, 5);
+
+    const searchItemElement6 = document.querySelector('.searchitem-6');
+    checkDefaultFilterList(searchItemElement6, 6);
+
+    
     // Toggle sub searchmenus and update dropdown icon
     $('.sub-btn').click(function() {
       //var subsearchmenu = $(this).next('.sub-searchmenu');
@@ -77,28 +157,6 @@ $(document).ready(function() {
         var searchmenuCheckboxAll = $('.searchmenu-checkboxall');
         searchmenuCheckboxAll.prop('checked', allCheckboxes.length === checkedCheckboxes.length);
       }
-  
-      // ... (your existing code here)
-  
-      $('#searchButton').click(function() {
-        var checkedValues = [];
-        $('.searchmenu-checkbox:checked').each(function() {
-          checkedValues.push($(this).val());
-        });
-        $('.sub-checkbox:checked').each(function() {
-          checkedValues.push($(this).val());
-        });
-        $('.bigsub-checkbox:checked').each(function() {
-          checkedValues.push($(this).val());
-        });
-        $('.child-sub-checkbox:checked').each(function() {
-          checkedValues.push($(this).val());
-        });
-  
-        console.log(checkedValues);
-      });
-  
-  
   });
   
 /*Search Menu End*/
