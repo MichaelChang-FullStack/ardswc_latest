25eb9180767ef621facbdc812864f205e51a508e
LucienLiou <lucien.liou@tarode.com>
2023-08-06 20:14:52 +0800

    Add book introduction

--- js/utils.js
+++ js/utils.js
@@ -89,3 +89,28 @@ function getDetailLink(resourceType, id){
   }
   return link + `?bookId=${id}`
 }
+
+function downloadResource(resourceType, id, fileName) {
+  switch (resourceType) {
+    case '圖書':
+      fetch(`/Files/Books/${id}/web/resources/_pdfs_/${id}__.pdf`)
+        .then(resp => resp.blob())
+        .then(blob => {
+            var url = window.URL.createObjectURL(blob);
+            var a = document.createElement('a');
+            a.style.display = 'none';
+            a.href = url;
+            a.download = `${fileName}.pdf`;
+            document.body.appendChild(a);
+            a.click();
+            window.URL.revokeObjectURL(url);
+        })
+        .catch(e => {
+          alert("此書本無法下載")
+          console.error(e)
+        });
+      break;
+    default:
+      break;
+  }
+}
