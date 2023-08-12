8f73caf8bae26fd6a1efaed3f601a845e1ee30cc
LucienLiou <lucien.liou@tarode.com>
2023-08-12 12:10:02 +0800

    Fix search resoult style

--- js/featureResource.js
+++ js/featureResource.js
@@ -16,7 +16,7 @@ $(document).ready(async function () {
     console.log({ resources })
 
     resources.forEach(resource => {
-        const { image, title, type, target, tags } = resource;
+        const { image, title, type, target, tags, BT_Name, BookID } = resource;
         const tagElement = tags.map((tag) => {
             return `
               <div class="frequest_search1">
@@ -28,30 +28,30 @@ $(document).ready(async function () {
             `
             <div class="card">            
                 <div class="mainbookinfo">
-                <div class="mainbookinfo_part1">
+                    <div class="mainbookinfo_part1">
                     <div class="mainbookinfo_part11"><span>${type ?? '教案'}</span></div>
                     <div class="mainbookinfo_part12"><img src="${image}" onError="this.onerror=null; this.src='./asset/images/search-result-default-img.png';" alt="${title}"></div>
                     </div>
-                <div class="mainbookinfo_part2">
-                    <div class="mainbookinfo_part21">
-                        <h4>${title}</h4>
-                    </div>
-                    <div class="mainbookinfo_part22">
-                      ${tagElement}
-                    </div>
-                    <div class="mainbookinfo_part23">
-                        <div class="">
-                            <img src="asset/images/Teacher_Edition_Home/icon_user.svg" alt="icon_user">
+                    <div class="mainbookinfo_part2">
+                        <div class="mainbookinfo_part21">
+                            <h4>${title}</h4>
                         </div>
-                        <div class="mainbookinfo_part23_2">
-                            <span>${target}</span>
+                        <div class="mainbookinfo_part22">
+                        ${tagElement}
                         </div>
-        
+                        <div class="mainbookinfo_part23">
+                            <div class="">
+                                <img src="asset/images/Teacher_Edition_Home/icon_user.svg" alt="icon_user">
+                            </div>
+                            <div class="mainbookinfo_part23_2">
+                                <span>${target}</span>
+                            </div>
+            
+                        </div>
+            
                     </div>
-        
                 </div>
-        
-            </div>
+                <a class="resource-detail" name=${title} href=${getDetailLink(BT_Name, BookID)}></a>
             </div>
             `
         )
@@ -59,53 +59,53 @@ $(document).ready(async function () {
 
 
     $('.feature-card-slider').slick({
-    // dots: true, // Disable default pagination dots
-    arrows: true,
-    slidesToShow: 3,
-    infinite: false,
-    responsive: [
-        {
-        breakpoint: 1600,
-        settings: {
-            slidesToShow: 3,
-            arrows: true,
-            adaptiveHeight: true
-        }
-        },
-        {
-        breakpoint: 1430,
-        settings: {
-            slidesToShow: 3,
-            arrows: true,
-            adaptiveHeight: true
-        }
-        },
-        {
-        breakpoint: 1060,
-        settings: {
-            slidesToShow: 2,
-            arrows: true,
-            adaptiveHeight: true
-        }
-        },
-        {
-        breakpoint: 750,
-        settings: {
-            slidesToShow: 1,
-            arrows: false,
-            dots: true,
-            adaptiveHeight: true
-        }
-        },
-        {
-        breakpoint: 385,
-        settings: {
-            slidesToShow: 1,
-            arrows: false,
-            dots: true,
-            adaptiveHeight: true
-        }
-        }
-    ]
+        // dots: true, // Disable default pagination dots
+        arrows: true,
+        slidesToShow: 3,
+        infinite: false,
+        responsive: [
+            {
+                breakpoint: 1600,
+                settings: {
+                    slidesToShow: 3,
+                    arrows: true,
+                    adaptiveHeight: true
+                }
+            },
+            {
+                breakpoint: 1430,
+                settings: {
+                    slidesToShow: 3,
+                    arrows: true,
+                    adaptiveHeight: true
+                }
+            },
+            {
+                breakpoint: 1060,
+                settings: {
+                    slidesToShow: 2,
+                    arrows: true,
+                    adaptiveHeight: true
+                }
+            },
+            {
+                breakpoint: 750,
+                settings: {
+                    slidesToShow: 1,
+                    arrows: false,
+                    dots: true,
+                    adaptiveHeight: true
+                }
+            },
+            {
+                breakpoint: 385,
+                settings: {
+                    slidesToShow: 1,
+                    arrows: false,
+                    dots: true,
+                    adaptiveHeight: true
+                }
+            }
+        ]
     });
 })
