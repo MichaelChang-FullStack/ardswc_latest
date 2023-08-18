a911a2d9c699c0ed4940399551eab72fb8df8c93
VishwaTarode <vishwa@tarode.com>
2023-08-18 19:50:23 +0530

    map Update 20230818

---
+++ component/filter/ADFilterModal_Map.js
@@ -0,0 +1,167 @@
+function ADFilter() {
+  return `
+  <div id="ad-filter" class="modal">
+    <div class="modal-content">
+    <span class="close" style="cursor: pointer;">&times;</span>
+    <div class="searchmenu">
+                        
+                        
+    <div class="searchtitle">
+        <h5>地區</h5>
+    </div>
+    <div class="searchitem searchitem-3 mainmenu searchitemmain">
+        <div class="searchmenudatapart mainmenutitle Menuinopen">
+            <input type="checkbox" class="checkbox searchmenu-checkbox" id="select-all-block-3" name="全選" value="全選">
+            <label class="sub-btnmain">全選<i class="fas fa-angle-right dropdown dropdown-3 rotateafter"></i></label>
+        </div>
+
+        <div class="sub-searchmenu sub-searchmenumain" style="display: block;">
+            <div class="searchmenudatapart">
+                <input type="checkbox" class="checkbox checkbox-block-3 child-sub-checkbox" id="resource47" name="北部" value="北部">
+                <label class="sub-searchitem">北部</label>
+            </div>
+            <div class="searchmenudatapart">
+                <input type="checkbox" class="checkbox checkbox-block-3 child-sub-checkbox" id="resource48" name="中部" value="中部">
+                <label class="sub-searchitem">中部</label>
+            </div>
+            <div class="searchmenudatapart">
+                <input type="checkbox" class="checkbox checkbox-block-3 child-sub-checkbox" id="resource49" name="南部" value="南部">
+                <label class="sub-searchitem">南部</label>
+            </div>
+            <div class="searchmenudatapart">
+                <input type="checkbox" class="checkbox checkbox-block-3 child-sub-checkbox" id="resource50" name="東部" value="東部">
+                <label class="sub-searchitem">東部</label>
+            </div>
+            
+            <div class="searchmenudatapart">
+                <input type="checkbox" class="checkbox checkbox-block-3 child-sub-checkbox" id="resource51" name="外島" value="外島">
+                <label class="sub-searchitem">外島</label>
+            </div>
+        </div>
+    </div>
+
+    <hr class="searchmenupartitionbar">
+    <div class="searchtitle">
+        <h5>類別</h5>
+    </div>
+    <div class="searchitem searchitem-2 mainmenu searchitemmain">
+        <div class="searchmenudatapart mainmenutitle Menuinopen">
+            <input type="checkbox" class="checkbox searchmenu-checkbox" id="select-all-block-2" name="全選" value="全選">
+            <label class="sub-btnmain">全選<i class="fas fa-angle-right dropdown dropdown-2 rotateafter"></i></label>
+        </div>
+        <div class="sub-searchmenu sub-searchmenumain" style="display: block;">
+            <div class="searchitem sub-searchitem-32 searchitemsub">
+                <div class="searchmenudatapart">
+                    <input type="checkbox" class="checkbox checkbox-block-2 bigsub-checkbox" id="resource32" name="酷學校家族" value="酷學校家族">
+                    <label class="sub-btn">酷學校家族<i class="fas fa-angle-right dropdown sub-dropdown-32 rotateafter"></i></label>
+                </div>
+                <div class="sub-searchmenu sub-searchmenusub" style="display: block;">
+                    <div class="searchmenudatapart">
+                        <input type="checkbox" class="checkbox checkbox-block-2 child-sub-checkbox" id="resource35" name="酷學校" value="酷學校">
+                        <label class="sub-searchitem"><svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <circle cx="10" cy="10" r="10" fill="#0884B7"></circle>
+                            <g clip-path="url(#clip0_2075_32806)">
+                            <path d="M16 8.5V9.5H13L10.5 7H14.5C14.8978 7 15.2794 7.15804 15.5607 7.43934C15.842 7.72064 16 8.10218 16 8.5ZM4 8.5V9.5H7L9.5 7H5.5C5.10218 7 4.72064 7.15804 4.43934 7.43934C4.15804 7.72064 4 8.10218 4 8.5ZM12.5 5.25L10.5 4H9.5V7H10.5V6.5L12.5 5.25ZM12.586 10.5H16V16H11.5V14C11.5 13.6022 11.342 13.2206 11.0607 12.9393C10.7794 12.658 10.3978 12.5 10 12.5C9.60217 12.5 9.22064 12.658 8.93934 12.9393C8.65803 13.2206 8.5 13.6022 8.5 14V16H4V10.5H7.414L10 8L12.586 10.5ZM11 10.5C11 10.3022 10.9413 10.1089 10.8315 9.94443C10.7216 9.77998 10.5654 9.65181 10.3827 9.57612C10.2 9.50043 9.99889 9.48063 9.80491 9.51921C9.61093 9.5578 9.43275 9.65304 9.29289 9.79289C9.15304 9.93275 9.0578 10.1109 9.01921 10.3049C8.98063 10.4989 9.00043 10.7 9.07612 10.8827C9.15181 11.0654 9.27998 11.2216 9.44443 11.3315C9.60888 11.4413 9.80222 11.5 10 11.5C10.2652 11.5 10.5196 11.3946 10.7071 11.2071C10.8946 11.0196 11 10.7652 11 10.5ZM10 13.5C9.86739 13.5 9.74021 13.5527 9.64645 13.6464C9.55268 13.7402 9.5 13.8674 9.5 14V16H10.5V14C10.5 13.8674 10.4473 13.7402 10.3536 13.6464C10.2598 13.5527 10.1326 13.5 10 13.5Z" fill="white"></path>
+                            </g>
+                            <defs>
+                            <clipPath id="clip0_2075_32806">
+                            <rect width="12" height="12" fill="white" transform="translate(4 4)"></rect>
+                            </clipPath>
+                            </defs>
+                            </svg>
+                            酷學校</label>
+                    </div>
+                    <div class="searchmenudatapart">
+                        <input type="checkbox" class="checkbox checkbox-block-2 child-sub-checkbox" id="resource36" name="推廣示範基地" value="推廣示範基地">
+                        <label class="sub-searchitem"><svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <circle cx="10" cy="10" r="10" fill="#008756"></circle>
+                            <g clip-path="url(#clip0_2075_32800)">
+                            <path d="M16 8.5V9.5H13L10.5 7H14.5C14.8978 7 15.2794 7.15804 15.5607 7.43934C15.842 7.72064 16 8.10218 16 8.5ZM4 8.5V9.5H7L9.5 7H5.5C5.10218 7 4.72064 7.15804 4.43934 7.43934C4.15804 7.72064 4 8.10218 4 8.5ZM12.5 5.25L10.5 4H9.5V7H10.5V6.5L12.5 5.25ZM12.586 10.5H16V16H11.5V14C11.5 13.6022 11.342 13.2206 11.0607 12.9393C10.7794 12.658 10.3978 12.5 10 12.5C9.60217 12.5 9.22064 12.658 8.93934 12.9393C8.65803 13.2206 8.5 13.6022 8.5 14V16H4V10.5H7.414L10 8L12.586 10.5ZM11 10.5C11 10.3022 10.9413 10.1089 10.8315 9.94443C10.7216 9.77998 10.5654 9.65181 10.3827 9.57612C10.2 9.50043 9.99889 9.48063 9.80491 9.51921C9.61093 9.5578 9.43275 9.65304 9.29289 9.79289C9.15304 9.93275 9.0578 10.1109 9.01921 10.3049C8.98063 10.4989 9.00043 10.7 9.07612 10.8827C9.15181 11.0654 9.27998 11.2216 9.44443 11.3315C9.60888 11.4413 9.80222 11.5 10 11.5C10.2652 11.5 10.5196 11.3946 10.7071 11.2071C10.8946 11.0196 11 10.7652 11 10.5ZM10 13.5C9.86739 13.5 9.74021 13.5527 9.64645 13.6464C9.55268 13.7402 9.5 13.8674 9.5 14V16H10.5V14C10.5 13.8674 10.4473 13.7402 10.3536 13.6464C10.2598 13.5527 10.1326 13.5 10 13.5Z" fill="white"></path>
+                            </g>
+                            <defs>
+                            <clipPath id="clip0_2075_32800">
+                            <rect width="12" height="12" fill="white" transform="translate(4 4)"></rect>
+                            </clipPath>
+                            </defs>
+                            </svg>推廣示範基地</label>
+                    </div>
+                </div>
+            </div>
+
+            <div class="searchmenudatapart">
+                <input type="checkbox" class="checkbox checkbox-block-3 child-sub-checkbox" id="resource37" name="戶外教室" value="戶外教室">
+                <label class="sub-searchitem"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                    <circle cx="10" cy="10" r="10" fill="#AC6B0F"></circle>
+                    <path d="M8.20686 5.33398C5.99431 5.33398 4.03638 11.1118 3.33398 14.0007H16.7673C16.4161 12.206 15.424 8.61681 14.265 8.61681C13.1061 8.61681 12.2017 11.068 11.8944 12.2936C11.5871 9.97371 10.4194 5.33398 8.20686 5.33398Z" fill="white"></path>
+                  </svg>戶外教室</label>
+            </div>
+        </div>
+    </div>
+
+</div>
+    </div>
+    <div class="btn-container">
+      <div class="btn-row">
+        <button id="clear" class="btn-14main">清除篩選</button>
+        <button id="filter" class="btn-14main">確認篩選</button>
+      </div>
+    </div>
+</div>
+    `
+}
+
+function toggleArrow(blockId) {
+  const arrow = document.querySelector(`.arrow-down-${blockId}`);
+  arrow.classList.toggle('active');
+
+  const checkboxBlock = document.querySelector(`.sub-checkbox-block-${blockId}`);
+  checkboxBlock.classList.toggle('active');
+
+  const hr = document.querySelector(`#ad-filter > div > div.show-sub-checkbox-block-${blockId} > hr`);
+  hr.classList.toggle('active');
+}
+
+document.getElementById("ad-filter-modal").innerHTML = ADFilter();
+
+
+var modal = document.getElementById("ad-filter");
+
+function openADFilterModal() {
+  const queryString = getQueryString();
+  const filterIds = queryString && queryString.filterId ? queryString.filterId.split(",") : [''];
+  document.body.style.overflow = "hidden";
+  modal.style.display = "block";
+  openDefaultFilter(filterIds)
+}
+
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    document.body.style.overflow = "auto";
+    modal.style.display = "none";
+  }
+}
+
+var clear = document.getElementById("clear");
+clear.onclick = function() {
+  var inputs = document.getElementsByTagName("input");
+  for (var i = 0; i < inputs.length; i++) {
+    if (inputs[i].type == "checkbox") {
+      inputs[i].checked = false;
+    }
+  }
+}
+
+
+
+$(document).ready(async function() {
+  var span = document.getElementsByClassName("close")[0];
+
+  span.onclick = function() {
+    modal.style.display = "none";
+    document.body.style.overflow = "auto";
+  }
+
+
+})
+
