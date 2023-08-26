4557a9cb3a642d4b716729a80ffb3d104dd40923
LucienLiou <lucien.liou@tarode.com>
2023-08-26 16:28:11 +0800

    Fix style

--- js/page_script_top_nav_bar.js
+++ js/page_script_top_nav_bar.js
@@ -119,7 +119,6 @@ $(function () {
                       <ul class="dropdown-menu sample">
                           <li><a href="/pages/Search_Result.html?filterId=6,7,8,9,10,13" class="menu-item">水保知識學</a></li>
                           <li><a href="/pages/Search_Result.html?filterId=17,18,19,20" class="menu-item">教學助手</a></li>
-                          <li><a href="#" class="menu-item">成果報告書</a></li>
                       </ul>
                   </li>
                   <li class="nav_partition">
