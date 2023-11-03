f12b7f5feac5ebc5738dfc0cf131ce7154c78f06
LucienLiou <lucien.liou@tarode.com>
2023-11-03 20:30:46 +0800

    Remove dead code

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-/opt/mssql/bin/sqlservr &
-
-sleep 30s
-
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'Swcb2022Learn' -i /var/opt/mssql/backup/restore-database.sql
-
-tail -f /dev/null
